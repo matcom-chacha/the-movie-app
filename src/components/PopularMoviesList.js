35a766a904c615b13245daaad495341dd15e57b9
matcom-chacha <gabybabuchi@gmail.com>
2023-08-09 02:22:23 -0400

    Add pagination to the Movie List Page

--- src/components/PopularMoviesList.js
+++ src/components/PopularMoviesList.js
@@ -7,49 +7,62 @@ import ImageListItemBar from "@mui/material/ImageListItemBar";
 import IconButton from "@mui/material/IconButton";
 import InfoIcon from "@mui/icons-material/Info";
 import { Link } from "react-router-dom";
+import Pagination from "@mui/material/Pagination";
 
 const PopularMoviesList = () => {
+  const [page, setPage] = React.useState(1);
   const popularMovies = useSelector((state) => state.movies.popular);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchPopularMovies());
-  }, [dispatch]);
+    dispatch(fetchPopularMovies(page));
+  }, [dispatch, page]);
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
 
   return (
-    <ImageList gap={16} cols={4} sx={{ margin: "0 auto", maxWidth: 1200 }}>
-      {popularMovies.map((movie) => (
-        <ImageListItem
-          key={movie.id}
-          sx={{
-            borderRadius: 5,
-            overflow: "hidden",
-          }}
-        >
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}?w=248&fit=crop&auto=format`}
-            srcSet={`https://image.tmdb.org/t/p/w500/${movie.poster_path}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={movie.title}
-            loading="lazy"
-            style={{ width: "100%", height: "auto" }}
-          />
-          <ImageListItemBar
-            title={movie.title}
-            subtitle={movie.release_date}
-            actionIcon={
-              <IconButton
-                sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                aria-label={`info about ${movie.title}`}
-                component={Link}
-                to={`/movies/${movie.id}`}
-              >
-                <InfoIcon />
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <div>
+      <ImageList gap={16} cols={4} sx={{ margin: "0 auto", maxWidth: 1200 }}>
+        {popularMovies.map((movie) => (
+          <ImageListItem
+            key={movie.id}
+            sx={{
+              borderRadius: 5,
+              overflow: "hidden",
+            }}
+          >
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}?w=248&fit=crop&auto=format`}
+              srcSet={`https://image.tmdb.org/t/p/w500/${movie.poster_path}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={movie.title}
+              loading="lazy"
+              style={{ width: "100%", height: "auto" }}
+            />
+            <ImageListItemBar
+              title={movie.title}
+              subtitle={movie.release_date}
+              actionIcon={
+                <IconButton
+                  sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                  aria-label={`info about ${movie.title}`}
+                  component={Link}
+                  to={`/movies/${movie.id}`}
+                >
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+      >
+        <Pagination count={10} page={page} onChange={handlePageChange} />
+      </div>
+    </div>
   );
 };
 
--- src/redux/moviesSlice.js
+++ src/redux/moviesSlice.js
@@ -5,9 +5,13 @@ const API_KEY = process.env.REACT_APP_TMD_API_KEY;
 
 export const fetchPopularMovies = createAsyncThunk(
   "movies/fetchPopular",
-  async () => {
+  async (page, { getState }) => {
+    const pages = getState().movies.pages;
+    if (pages[page]) {
+      return pages[page];
+    }
     const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`
     );
     return response.data.results;
   }
@@ -15,7 +19,7 @@ export const fetchPopularMovies = createAsyncThunk(
 
 const moviesSlice = createSlice({
   name: "movies",
-  initialState: { popular: [], status: "idle", error: null },
+  initialState: { popular: [], pages: {}, status: "idle", error: null },
   reducers: {},
   extraReducers: (builder) => {
     builder
@@ -26,6 +30,8 @@ const moviesSlice = createSlice({
         state.status = "succeeded";
         // Add fetched movies to the array
         state.popular = action.payload;
+        // Store the fetched page as action.meta.arg resturns the number
+        state.pages[action.meta.arg] = action.payload;
       })
       .addCase(fetchPopularMovies.rejected, (state, action) => {
         state.status = "failed";
